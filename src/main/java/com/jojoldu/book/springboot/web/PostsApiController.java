dc28a90fc8c015e9fdff0cd6441bd95c3dbcb385
justCarryOn <train1036@gmail.com>
2021-03-25 17:19:31 +0900

    Mustache CRUD

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -15,7 +15,8 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-        return  postsService.save(requestDto);
+        System.out.println("Request Check : " + requestDto.toEntity());
+        return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
@@ -24,7 +25,13 @@ public class PostsApiController {
     }
 
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id) {
+    public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 }
--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -18,6 +18,7 @@ public class PostsSaveRequestDto {
         this.author = author;
     }
 
+
     public Posts toEntity() {
         return Posts.builder()
                 .title(title)
